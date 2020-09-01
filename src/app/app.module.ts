cb79736a77018e88f5600be4832592c150ed994e
oscar saavedra <oscar@DESKTOP-6B46A0J.localdomain>
2020-09-01 17:28:43 -0500

    componente de detalle pregunta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { QuestionDetailComponent } from './question-detail/question-detail.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionDetailComponent
   ],
   imports: [
     BrowserModule,
